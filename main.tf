9f04cc879b06811a817558f325122a588c457761
Kyaw Cthu <kyawcthu@mymacbook.local>
2024-01-27 16:36:02 +0400

    Changes made to remote backend variable

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 module "myremotebackend" {
   source = "git::git@github.com:rosaliei/tf-aws-github.git"
-  name = "xxx"
+  backend_s3_bucket_name_kst = "kst-s3"
+  backend_dynamodb_table_name_kst = "kst-dynamodb"
 }
--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 # own remote-backend variables
-variable "backend_s3_bucket_name" {
+variable "backend_s3_bucket_name_kst" {
   description = "AWS S3 Bucket Name"
   type        = string
   default     = "aws-s3-backend-tfstate"
 }
-variable "backend_dynamodb_table_name" {
+variable "backend_dynamodb_table_name_kst" {
   description = "AWS DynamoDB Table Name"
   type        = string
   default     = "aws-dynamodb-backend-tfstate"
