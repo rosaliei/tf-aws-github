3b1366e89ad221f2bd2203d72dc0d944e62a67bd
Kyaw Cthu <kyawcthu@mymacbook.local>
2024-01-27 14:14:11 +0400

    PUSHING FILE

---
+++ versions.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.34.0"
+    }
+  }
+
+  #backend "s3" {
+  #   bucket         = "bca-aws-master-tfstate"
+  #   key            = "terraform.tfstate"
+  #   region         = "ap-southeast-1"
+  #   encrypt        = true
+  #   profile        = "kst-dev" # this line is required
+  #   dynamodb_table = "bca-aws-master-tfstate-locks"
+  #}
+}
+
+provider "aws" {
+  shared_config_files      = ["$HOME/.aws/config"]
+  shared_credentials_files = ["$HOME/.aws/credentials"]
+  profile                  = "kst-dev"
+  #alias                   = "kst-dev"
+  region                   = "ap-southeast-1"
+}
