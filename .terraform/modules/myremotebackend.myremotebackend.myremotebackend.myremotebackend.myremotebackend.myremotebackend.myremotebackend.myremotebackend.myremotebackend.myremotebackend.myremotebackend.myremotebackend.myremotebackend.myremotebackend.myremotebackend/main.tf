af292ffc5b89cef2f8a7e78027bef9bad476a211
Kyaw Cthu <kyawcthu@mymacbook.local>
2024-01-27 16:48:00 +0400

    s3_test module name change

--- .terraform/modules/myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend/main.tf
+++ .terraform/modules/myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend/main.tf
@@ -1,4 +1,4 @@
-module "s3_test" {
+module "myremotebackend" {
   source = "git::git@github.com:rosaliei/tf-aws-github.git"
   backend_s3_bucket_name_kst = "kst-s3"
   backend_dynamodb_table_name_kst = "kst-dynamodb"
