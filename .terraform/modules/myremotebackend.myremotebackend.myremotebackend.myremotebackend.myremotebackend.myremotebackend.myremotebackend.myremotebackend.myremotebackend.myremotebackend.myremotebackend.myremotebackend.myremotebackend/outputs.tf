6c9c906d6fd3a0f27f2f2c0a1fc74890b795a854
Kyaw Cthu <kyawcthu@mymacbook.local>
2024-01-27 16:44:39 +0400

    module name changte

--- .terraform/modules/myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "public_subnets_id" {
-    description = "Public Subnets ID"
-    value = module.vpc.public_subnets
-}
-
-output "dynamodb-arn" {
-    description = "dynamodb arn"
-    value = aws_dynamodb_table.terraform_locks.id
-}
--- .terraform/modules/myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend.myremotebackend/main.tf
+++
@@ -1,5 +0,0 @@
-module "myremotebackend" {
-  source = "git::git@github.com:rosaliei/tf-aws-github.git"
-  backend_s3_bucket_name_kst = "kst-s3"
-  backend_dynamodb_table_name_kst = "kst-dynamodb"
-}
